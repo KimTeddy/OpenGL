3e5cb647b9f21e7204510af74301f8f5ca9f9d79
TeddyElectronics <kyc20126@gmail.com>
2024-01-17 14:42:34 +0900

    glutReshapeWindow

--- 230324_PONG_by_KimTeddy/230324_PONG_by_KimTeddy.cpp
+++ 230324_PONG_by_KimTeddy/230324_PONG_by_KimTeddy.cpp
@@ -14,6 +14,7 @@ char Health[20] = "";
 char Gameover[] = "G A M E O V E R";
 char GameoverMessage[] = "PRESS <R> TO REPLAY.";
 char GameoverExit[] = "PRESS <E> TO EXIT.";
+char link[] = "https://github.com/KimTeddy/";
 
 int score = 0;
 int health = 3;
@@ -22,6 +23,11 @@ int game_speed = 2;
 float bar_rgb[3] = { 0 };
 float sphere_rgb[3] = { 0.0, 0.5, 0.8 };
 short darkmode = 0;
+int wh = 800;
+void changeSize(int w, int h) {
+	glutReshapeWindow(wh, wh);
+}
+
 
 void Menu(int entityID)
 {
@@ -46,7 +52,6 @@ void Init() {
 	glutAddMenuEntry("다크모드 OFF", 1);
 	glutAddMenuEntry("다크모드 ON", 2);
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
-
 }
 void renderStrokeString(float x, float y, float z, float R, float G, float B, float scale, void* font, char* string)
 {
@@ -78,7 +83,7 @@ void Disp() {
 		glTranslatef(DeltaX, DeltaY, 0.0);
 		glColor3f(sphere_rgb[0], sphere_rgb[1], sphere_rgb[2]);
 		//glutSolidSphere(0.04, 30, 30);
-		GLUquadric * sphere = gluNewQuadric();
+		GLUquadric* sphere = gluNewQuadric();
 		gluQuadricDrawStyle(sphere, GLU_FILL);
 		gluQuadricNormals(sphere, GLU_SMOOTH);
 		gluQuadricOrientation(sphere, GLU_OUTSIDE);
@@ -104,7 +109,7 @@ void Disp() {
 		gluQuadricOrientation(sphere2, GLU_OUTSIDE);
 		gluSphere(sphere2, 0.04, 20, 20);
 	glPopMatrix();
-	
+
 	pre2DeltaX = pre1DeltaX;
 	pre1DeltaX = DeltaX;
 
@@ -113,14 +118,12 @@ void Disp() {
 	//glColor3f(0.0, 0.5, 0.8);
 	//glRectf(0.0 + DeltaX, -0.025 + DeltaY, 
 	//		0.05 + DeltaX, 0.025 + DeltaY);
-	
+
 	glColor3f(bar_rgb[0], bar_rgb[1], bar_rgb[2]);//bar
 	glRectf(0.15, barY + 0.2,
-			0.2, barY - 0.2);
+		0.2, barY - 0.2);
 	//renderStrokeString(0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, GLUT_STROKE_MONO_ROMAN, mystring);
 	glRasterPos2f(0, 0.9);
-
-
 
 	if (darkmode == 0)
 	{
@@ -137,6 +140,7 @@ void Disp() {
 		else if (health == 0) glClearColor(0.5, 0.0, 0.0, 1.0);
 	}
 	renderBitmapString(0.85, 0.9, 0.5, 0.5, 0.5, GLUT_BITMAP_HELVETICA_18, Title);
+	renderBitmapString(1.35, ((float)(-350) / (float)wh), 0.8, 0.8, 0.8, GLUT_BITMAP_HELVETICA_18, link);
 	//glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, 'A');
 	if (gameover == 0) {
 		sprintf_s(Score, "SCORE : %d", score);
@@ -151,8 +155,6 @@ void Disp() {
 		game_speed = 100;
 		glClearColor(0.0, 0.0, 0.0, 0.5);
 	}
-	
-	
 	//glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, 'A');
 	//glFlush();
 	glutSwapBuffers();
@@ -190,14 +192,14 @@ void Key(unsigned char key, int x, int y)
 void Mouse(int x, int y)
 {
 	barY = -(float)y * 0.01 + 2.5;
-	if(barY >= -0.8 && barY <= 0.8)
+	if (barY >= -0.8 && barY <= 0.8)
 	{
-		barY = -(float)y * 0.01 +2.5 ;
-		
+		barY = -(float)y * 0.01 + 2.5;
+
 		//barY = 250.0 -((float)y)*0.05;
 	}
 	else if (barY < -0.8) barY = -0.8;
-	else if (barY >  0.8) barY = 0.8;
+	else if (barY > 0.8) barY = 0.8;
 	glutPostRedisplay();
 }
 
@@ -222,7 +224,7 @@ void Timer(int Value)
 		DeltaY = DeltaY + dy;
 	}
 
-	if (DeltaX >= 1.95) 
+	if (DeltaX >= 1.95)
 	{
 		dx = -0.0025;
 	}
@@ -230,7 +232,7 @@ void Timer(int Value)
 	{
 		dx = 0.0025;
 		score += 5;
-		if(darkmode == 0)
+		if (darkmode == 0)
 		{
 			bar_rgb[0] = (float)(rand() % 5) * 0.1;
 			bar_rgb[1] = (float)(rand() % 5) * 0.1;
@@ -251,7 +253,7 @@ void Timer(int Value)
 			sphere_rgb[2] = 1.0 - (float)(rand() % 5) * 0.1;
 		}
 	}
-	if (DeltaX <= 0.0) 
+	if (DeltaX <= 0.0)
 	{
 		dx = 0.05;
 		if (score >= 0)
@@ -266,11 +268,11 @@ void Timer(int Value)
 			gameover = 1;
 		}
 	}
-	if (DeltaY >= 0.95) 
+	if (DeltaY >= 0.95)
 	{
 		dy = -0.002;
 	}
-	if (DeltaY <= -0.95) 
+	if (DeltaY <= -0.95)
 	{
 		dy = 0.002;
 	}
@@ -288,7 +290,10 @@ int main(int argc, char** argv)
 	glutInitWindowPosition(500, 10);
 	glutCreateWindow("                                                                                     P  O  N  G");
 	Init();
+
 	glutDisplayFunc(Disp);
+	glutReshapeFunc(changeSize);
+
 	glutKeyboardFunc(Key);
 	glutPassiveMotionFunc(Mouse);
 	glutTimerFunc(game_speed, Timer, 1);
